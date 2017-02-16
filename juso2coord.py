b15491ef2186236fdea1820701226261e63c4c2e
Yoonku Hwang <yz0624@gmail.com>
2017-02-16 19:34:35 +0900

    update

--- juso2coord.py
+++ juso2coord.py
@@ -12,7 +12,7 @@ qeury_args = {'query' : '서울 마포구 노고산동 31-11',
 
 info = []
 df = pd.read_csv('requested_places.csv')
-for index, row in df.iterrows():
+for idx, row in df.iterrows():
 	encText = urllib.parse.quote(row['주소'])
 	url = "https://openapi.naver.com/v1/map/geocode?query=" + encText
 	request = urllib.request.Request(url)
@@ -20,14 +20,18 @@ for index, row in df.iterrows():
 	request.add_header("X-Naver-Client-Secret",client_secret)
 	response = urllib.request.urlopen(request)
 	rescode = response.getcode()
-	response_body = response.read()
-	content = response_body.decode('utf-8')
-	dic = json.loads(content)
-	name = row['상호명']
-	lat = dic['result']['items'][0]['point']['y']
-	lon = dic['result']['items'][0]['point']['x']
-	item = {"name" : name, "lat" : lat, "lon" : lon, "type" : row['업종'] }
-	info.append(item)
-
+	if(rescode is 200):
+		response_body = response.read()
+		content = response_body.decode('utf-8')
+		dic = json.loads(content)
+		name = row['상호명']
+		lat = dic['result']['items'][0]['point']['y']
+		lon = dic['result']['items'][0]['point']['x']
+		item = {"name" : name, "lat" : lat, "lon" : lon, "type" : row['업종'] }
+		info.append(item)
+		df = df.drop(df.index[[idx]])
+	else:
+		print("Error Code:" + rescode)
 pprint(info)
 pd.DataFrame(info).to_csv('data.csv', encoding='utf8', index=False)
+df.to_csv('requested_places.csv', encoding='utf8', index=False)
